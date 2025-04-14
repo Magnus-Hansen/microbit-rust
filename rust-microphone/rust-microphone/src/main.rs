905d5eb1f184cefe412a36ec362660c7c794ac87
Magnus-Hansen <54136904+Magnus-Hansen@users.noreply.github.com>
2025-04-14 23:55:20 +0200

    fix

--- rust-microphone/rust-microphone/src/main.rs
+++ rust-microphone/rust-microphone/src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use defmt_rtt as _;
+use panic_halt as _;
 
 use cortex_m_rt::entry;
 use microbit::{
@@ -16,9 +18,8 @@ use defmt::println;
 #[entry]
 fn main() -> ! {
     if let Some(board) = Board::take() {
-
         let saadc_config = SaadcConfig::default();
-        let mut  saadc = Saadc::new(board.ADC, saadc_config);
+        let mut saadc = Saadc::new(board.ADC, saadc_config);
         let mut mic_in = board.microphone_pins.mic_in.into_floating_input();
 
         board
@@ -33,13 +34,5 @@ fn main() -> ! {
             println!("mic value: {}", mic_value);
         }
     }
-    panic!("End");
+    panic!("End!")
 }
-
-/*fn measure_and_print() {
-        let mic_value = saadc
-            .read_channel(&mut mic_in)
-            .expect("could not read value of microphone") as i32;
-        println!("mic value: {}", mic_value);
-}
-*/
