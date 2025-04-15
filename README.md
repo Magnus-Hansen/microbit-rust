65e49891d315402173a2c713ef6ca38ca84fafb7
Magnus Ansbjerg Selchou Hansen <mah012@edu.zealand.dk>
2025-04-15 20:56:03 +0200

    udp

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
-microbit common og v2 er bibloteker jeg bruger til rust-microphone
-
-rust microphone er forsøg på at få microbit med rust til at virke 
-
-lige nu bruger microbit et python script og serial read læser fra den
-
-
 for at køre rust-microphone installer:
 
 cargo install probe-rs-tools flip-link
--- serialRead/src/main.rs
+++ serialRead/src/main.rs
@@ -1,20 +1,30 @@
-use serialport::SerialPort;
 use std::time::Duration;
 use std::io::BufReader;
 use std::io::BufRead;
+use std::net::UdpSocket;
 
-fn main() {
-    let mut serial_port = serialport::new("COM6", 115200)
+fn main() -> std::io::Result<()> {
+    //let serial_port = serialport::new("COM6", 115200)
+    let serial_port = serialport::new("COM3", 115200)
         .timeout(Duration::from_millis(4000))
         .open()
         .expect("Failed to open serial port");
 
+    let socket = UdpSocket::bind("0.0.0.0:0")?;
+    let target_adress = "127.0.0.1:8080";
+
     let mut reader = BufReader::new(serial_port);
-    let mut my_str = String::new();
 
     loop {
-        reader.read_line(&mut my_str).unwrap();
+        let mut sound_str = String::new();
+        reader.read_line(&mut sound_str).unwrap();
 
-        println!("{}", my_str);
+        let sound = match sound_str.trim().parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("{}", sound);
+        socket.send_to(&sound.to_be_bytes(), target_adress)?;
+
     }
 }
